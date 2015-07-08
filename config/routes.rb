d093b7ba0cfdc510d581d7c2bfeb829b6d6f9c4b
Jasiek <jasiek92@gmail.com>
2015-07-08 21:00:51 +0200

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
+
   root 'posts#index'
 end
